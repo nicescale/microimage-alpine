6a4280b587ee432214789e2e97b6ed5de9986264
shellrock <hanwoody@gmail.com>
2015-08-27 12:30:32 +0800

    build latest tag

--- build.sh
+++ build.sh
@@ -7,16 +7,13 @@ export REG_URL=d.nicescale.com:5000
 export IMAGE=alpine
 export VERSION=3.2
 
-docker build -t $REG_URL/microimages/$IMAGE:$VERSION .
+docker build -t $REG_URL/microimages/$IMAGE .
 
 ./test.sh
 
-docker exec $CON_NAME /bin/echo "testing"
-
-echo "---> The test pass"
-
 echo "---> Starting push $REG_URL/microimages/$IMAGE:$VERSION"
 
-docker push $REG_URL/microimages/$IMAGE:$VERSION
+docker tag -f $REG_URL/microimages/$IMAGE $REG_URL/microimages/$IMAGE:$VERSION
 
-docker rm -f $CON_NAME
+docker push $REG_URL/microimages/$IMAGE
+
